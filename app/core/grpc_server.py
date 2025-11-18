5b34cfa7f274440fa6bc62c4aa279e6041358dd4
QBking <wangyinan@2033ai.com>
2025-11-18 17:09:54 +0800

    grpc健康检查

--- app/core/grpc_server.py
+++ app/core/grpc_server.py
@@ -1,5 +1,6 @@
 from concurrent import futures
 import grpc
+from grpc_health.v1 import health, health_pb2, health_pb2_grpc
 from ..routers.grpc import register_grpc_services
 from .config import settings
 from .log import logger
@@ -13,6 +14,12 @@ def start_grpc_server():
 
     # 注册所有 gRPC 服务
     register_grpc_services(grpc_server)
+
+    # 注册健康检查服务
+    health_servicer = health.HealthServicer()
+    health_pb2_grpc.add_HealthServicer_to_server(health_servicer, grpc_server)
+    # 设置某个服务名为健康，"" 表示整个 gRPC 服务
+    health_servicer.set("", health_pb2.HealthCheckResponse.SERVING)
 
     grpc_address = f"{settings.GRPC_SERVER_HOST}:{settings.GRPC_SERVER_PORT}"
     grpc_server.add_insecure_port(grpc_address)
